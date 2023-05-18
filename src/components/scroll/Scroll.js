6ddbbb4c5312be5bbd2d1ac1b608ef42efff3de9
steno1 <onuprinceley@gmail.com>
2023-05-18 15:27:22 +0100

    update code

--- src/components/scroll/Scroll.js
+++ src/components/scroll/Scroll.js
@@ -1,12 +1,17 @@
-const Scroll=(props)=>{
+const Scroll=({children})=>{
 return (
     <div style={{
         overflowY:"scroll",
         height:"100vh"
     }}>
-    {props.children}
+    {children}
 
     </div>
 )
 }
 export default Scroll;
+/*
+In React, children is a special prop that allows you
+ to pass components, elements, or text as children to a component.
+  It provides a way to nest content within a component.
+*/
